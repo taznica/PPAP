9a5192d21fdecac68c684de506d3a0a9c202d699
ttsutchi <tailegog@gmail.com>
2017-01-24 00:46:20 +0900

    Create PPAPGuiContainer.java

---
+++ src/main/java/com/example/examplemod/PPAPGuiContainer.java
@@ -0,0 +1,31 @@
+package com.example.examplemod;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.util.ResourceLocation;
+
+public class PPAPGuiContainer extends GuiContainer {
+    private static final ResourceLocation TEXTURE = new ResourceLocation("<DomainName>", "textures/gui/gui_texture.png");
+
+    public PPAPGuiContainer(int x, int y, int z) {
+        super(new PPAPContainer(x, y, z));
+    }
+
+    // 文字等の描画
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseZ) {
+        super.drawGuiContainerForegroundLayer(mouseX, mouseZ);
+    }
+
+    // 背景の描画
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTick, int mouseX, int mouseZ) {
+        this.mc.renderEngine.bindTexture(TEXTURE);
+        this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, xSize, ySize);
+    }
+
+    // ゲームの処理を止めるかどうか
+    @Override
+    public boolean doesGuiPauseGame() {
+        return false;
+    }
+}
