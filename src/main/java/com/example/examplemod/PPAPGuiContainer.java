504e54ca32843192fdf8893b79b69e11527597fa
ttsutchi <tailegog@gmail.com>
2017-01-24 01:28:50 +0900

    Change location of TEXTURE in PPAPGuiContainer.java

--- src/main/java/com/example/examplemod/PPAPGuiContainer.java
+++ src/main/java/com/example/examplemod/PPAPGuiContainer.java
@@ -4,7 +4,8 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.util.ResourceLocation;
 
 public class PPAPGuiContainer extends GuiContainer {
-    private static final ResourceLocation TEXTURE = new ResourceLocation("<DomainName>", "textures/gui/gui_texture.png");
+//    private static final ResourceLocation TEXTURE = new ResourceLocation("<DomainName>", "textures/gui/gui_texture.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation("textures/gui/container/generic_54.png");
 
     public PPAPGuiContainer(int x, int y, int z) {
         super(new PPAPContainer(x, y, z));
