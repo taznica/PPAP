e31a258ca62efd6c537a35cdcea1a44db8098fa6
ttsutchi <tailegog@gmail.com>
2017-01-23 23:57:05 +0900

    Register INSTANCE and GUI_ID

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,7 +10,10 @@ public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
-    
+
+    public static ExampleMod INSTANCE;
+    public static final int GUI_ID = 0;
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
